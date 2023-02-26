8f1293f93ab1821144b78ac81fc48f0a64953985
Nishal Nepal <nepalnishal4@gmail.com>
2023-02-26 14:18:24 -0500

    Made changes to test.py to get temp

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 #!/bin/python3
 
-print("testing")
+from bs4 import BeautifulSoup as bs
+import requests as rq
 
+url = "https://weather.com/weather/today"
+w_html = rq.get(url)
+bsed_w_html = bs(w_html.text, "html.parser")
+if bsed_w_html:
+    print("Website Read")
+else:
+    print("Cannot Read Website")
+    exit()
+temp_rn = bsed_w_html.find(["span"], class_="CurrentConditions--tempValue--MHmYY")
+print(temp_rn)
