e85b1c95657c991a26f87f26e925dc924346c883
Nishal Nepal <nepalnishal4@gmail.com>
2023-04-04 16:02:35 -0400

    Made modifications to the analysis.py file b/c the graph was very weird, realized that I forgot to convert the humidity to integer

--- analysis.py
+++ analysis.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 weather_df = pd.read_csv("weather_data.csv")
-weather_df.head()
 
 # Separating time to another column
 
@@ -17,21 +16,13 @@ weather_df.date = weather_df.date.str[:9]
 
 # Lets first change the percentage humidity to just a number
 weather_df["humidity"] = weather_df["humidity"].str[:2]   # Leveraging the fact that percentage never falls below double digits
+weather_df["humidity"] = weather_df["humidity"].astype(int)  # Changing the string to int; could cause problems later.
 
 humidity_axis = weather_df.humidity
-# I am doing this because the graph looks better this way, the script I used collected
-# data every 30 min, so if you know start time, you will be able to calculate what
-# time the data was collected
-time_axis = [i*30 for i in range(1, 336)]
 
-
-# The humidity axis' ticks look really cluttered, so I will make my own ticks
-humidity_ticks = [i*10 for i in range(11)]
-
-plt.plot(time_axis, humidity_axis)
 plt.title("Humidty/Time in one week")
 plt.xlabel("Time in minutes")
-plt.yticks(humidity_ticks)  # changing ticks to make it look more visually appealing
 plt.ylabel("Humidity in %")
+plt.scatter(np.arange(0, 10050, step=30), humidity_axis)  # Making the time increment in 30 min so that plotting this is easier
 
 plt.show
